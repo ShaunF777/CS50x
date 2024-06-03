86a94ffbd630051dfe6c065fac3faccf86740863
ShaunF777 <shaunfr777@gmail.com>
2024-06-03 21:11:39 +0200

    Update 01hello.c

--- Week1-C/01hello.c
+++ Week1-C/01hello.c
@@ -1,1 +1,9 @@
+#include <cs50.h>
+#include <stdio.h>
+
+int main(void)
+{
+    string answer = get_string("What's your name? "); //get_string saves the return value
+    printf("hello, %s\n", answer); 
+}
 
