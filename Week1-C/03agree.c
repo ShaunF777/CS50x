dd25bf21e8fc1f7df3093e15d2aca13fadd5055f
Shaun Fourie <shaunfr777@gmail.com>
2024-06-07 10:45:58 +0200

    Update 03agree.c

--- Week1-C/03agree.c
+++ Week1-C/03agree.c
@@ -5,9 +5,9 @@ int main(void)
 {
     char c = get_char("Do you agree? ");
 
-    if  (c == 'y' || c == 'Y')
-    {
-        printf("Agreed.\n");
+       if  (c == 'y' || c == 'Y')   //Ternary operator can be used to sharthand this
+                                    // (condition)          ?   then execution     :   else execution
+                                    //if( c=='y' || c=='Y') ? printf("Agreed.\n")   : if (c == 'n' || c == 'N') ? printf("Not agreed.\n") : ;
     }
     else if (c == 'n' || c == 'N')
     {
