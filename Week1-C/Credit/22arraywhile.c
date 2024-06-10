901419c2995b5577ff11e76885fb0ba6d9528ce1
Shaun Fourie <shaunfr777@gmail.com>
2024-06-10 12:00:39 +0200

    Create 22arraywhile.c

---
+++ Week1-C/Credit/22arraywhile.c
@@ -0,0 +1,50 @@
+#include <cs50.h>
+#include <stdio.h>
+#define MAX 17
+
+void printDigit(long N);
+
+int main()
+{
+    long cardn;
+    cardn = get_long("Credit card number: ");
+
+    printDigit(cardn);
+    printf("\n");
+    return 0;
+
+}
+// Function to print the digit of
+// number N
+void printDigit(long N)
+{
+    // To store the digit of the number N
+    int arr[MAX];
+    int i = 0;
+    int j, r;
+
+    // Till N becomes 0
+    while (N != 0)
+    {
+        // Extract the last digit of N
+        r = N % 10;
+
+        // Put the digit in arr[]
+        arr[i] = r;
+        i++;
+
+        // Update N to N/10 to extract
+        // next last digit
+        N = N / 10;
+    }
+
+    // Print the digit of N by traversing
+    // arr[] reverse
+    for (j = i - 1; j > -1; j--)
+    {
+        printf("%d ", arr[j]);
+    }
+
+}
+
+
