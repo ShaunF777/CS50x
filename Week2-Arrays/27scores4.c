aa56fa32687221f450df4e40d9f16556e20ae51c
Shaun Fourie <shaunfr777@gmail.com>
2024-06-10 15:00:06 +0200

    Update 27scores4.c

--- Week2-Arrays/27scores4.c
+++ Week2-Arrays/27scores4.c
@@ -4,22 +4,23 @@
 #include <stdio.h>
 
 // Constant
-const int N = 3;
+const int N = 3; // declared a contant N = 3
 
-// Prototype
-float average(int length, int array[]);
+// Prototype, 
+float average(int length, int array[]); // introduce average function, will return a float, giving it the array and the length thereof
 
 int main(void)
 {
     // Get scores
-    int scores[N];
-    for (int i = 0; i < N; i++)
+    int scores[N];  // declaring var array scores, with N size
+    for (int i = 0; i < N; i++) //iterating from i to N
     {
-        scores[i] = get_int("Score: ");
+        scores[i] = get_int("Score: "); // asking as they get plugged into scores
     }
 
     // Print average
-    printf("Average: %f\n", average(N, scores));
+    printf("Average: %f\n", average(N, scores)); // print out the average, automatically by
+    // calling av function, pass in array length and the array itself, hoping that it'll return float to printf
 }
 
 float average(int length, int array[])
@@ -28,8 +29,10 @@ float average(int length, int array[])
     int sum = 0;
     for (int i = 0; i < length; i++)
     {
-        sum += array[i];
+        sum += array[i]; // sum  = sum + array[i];
     }
-    return sum / (float) length;
+    return sum / (float) length;   //sum of N array entries / array length
 }
+
+
 
