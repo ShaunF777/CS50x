427ba15badbf9cc4eb2a20181c4502e6766b092b
Shaun Fourie <shaunfr777@gmail.com>
2024-06-14 16:17:50 +0200

    Update 29alphabet.c

--- Week2-Arrays/29alphabet.c
+++ Week2-Arrays/29alphabet.c
@@ -1,22 +1,32 @@
-// Section video - Doubling up
-// Create an array of integers in which each integer is 2 time the value of the previouse integer
-// The first element is 1.
-// Print the array, integer by integer
 #include <cs50.h>
 #include <stdio.h>
+#include <string.h> // strlen function works from this header
 
 int main(void)
 {
-    int size = get_int(" Enter size:"); // get user input for size
-    int sequence[size];
-    sequence[0] = 1;
-    printf("%i\n", sequence[0]);
-
-    // can be initialised with sequence index 0 at value 1 for (int i = 0, sequence[5]=1; i<s; i++)
-    for (int i = 1; i<size; i++)
+    string letters = get_string(" Enter letters: ");
+    int length = strlen(letters);
+    for (int i = 0; i < length -1 ;i++) // length -1 to stop at last char of letters, and not go further
     {
-        sequence[i] = sequence[i-1] * 2; // multiply whatever is in the index of array
-    //  sequence[1] = sequence[0] wich has value 1 * 2, so now index gets the element 2 stored in it
-        printf("%i\n", sequence[i]);
+        // printf("%i ", letters[i]);
+        //check if characters are in alphabetical order
+        if (letters[i] < letters[i+1])
+        {
+            printf("%i ", i);
+            printf("%c ", letters[i]);
+            printf("%i ", letters[i]);
+            printf("In order \n");
+            // return 0; // this tells the program to quit
+        }
+        else
+        {
+            printf("%i ", i);
+            printf("%c ", letters[i]);
+            printf("%i ", letters[i]);
+            printf("Not in order \n");
+        }
     }
+    //printf("Alphabetical order\n");
+    //return 0;
 }
+
